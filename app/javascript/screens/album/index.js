f1cfff119997d3fbee39dabbe1219507bf9216ef
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-04-20 17:33:32 -0300

    feat: inclusão do navebar e ajuste na tela inicial(discovery)

--- app/javascript/screens/album/index.js
+++ app/javascript/screens/album/index.js
@@ -1,10 +1,12 @@
 import React, { Fragment, useState } from 'react';
+import NavbarFooter from '../../components/common/navbar_footer';
 
 const AlbumScreen = () => {
     return(
         <Fragment>
-            Album Screen
+            <div> Favorites Screen</div>
+            <NavbarFooter/>
         </Fragment>
-)
+    )
 }
 export default AlbumScreen;
