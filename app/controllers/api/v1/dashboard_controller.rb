72d57d20b9a751d4a70300512a63d4fff6ed4916
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-04-21 19:43:32 -0300

    feat: insercao dos albuns no dashboard(12albums)

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -13,9 +13,9 @@ class Api::V1::DashboardController < ApplicationController
     heard_categories = @recent_albums.map(&:category)
     if heard_categories.present?
       @recommended_albums = Album.joins(:category, :songs).where(category: heard_categories).order("songs.played_count")
-                                 .select("distinct albums.*").limit(2)
+                                 .select("distinct albums.*").limit(12)
     else
-      @recommended_albums = Album.all.limit(2)
+      @recommended_albums = Album.all.limit(12)
     end
   end
 end
