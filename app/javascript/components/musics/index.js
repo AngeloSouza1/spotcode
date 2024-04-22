8c358a266234e64f34fd32a54c673aad24cc539c
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-04-22 10:16:39 -0300

    feat: inserção das musicas, autores e ajuste do botao player

--- app/javascript/components/musics/index.js
+++ app/javascript/components/musics/index.js
@@ -1,13 +1,30 @@
-import React, { Fragment } from 'react';
-import styled from 'styled-components';
+import React, { Fragment, useState, useEffect } from 'react';
 import Music from './music';
 import { Button, Columns } from 'react-bulma-components';
-
+import styled from 'styled-components';
 
 const PlaySequenceButton = styled(Button)`
-  margin-bottom: 30px;
+  margin-bottom: 28px;
   `
+
+
 const Musics = (props) => {
+    const [songs, setSongs] = useState([]);
+    const [playing, setPlaying] = useState([]);
+
+    useEffect(() => {
+        setSongs(props.songs.map((song, key) =>
+            <Music
+                song={song}
+                playing={playing.id == song.id}
+                setPlaying={setPlaying}
+                key={key}
+            />
+        ));
+    }, [props.songs, playing]);
+
+
+
     return (
         <Fragment>
             <Columns className='is-mobile is-centered'>
@@ -21,9 +38,7 @@ const Musics = (props) => {
                 </Columns.Column>
             </Columns>
             {/* Musics :) */}
-            <Music />
-            <Music />
-            <Music />
+            {songs}
         </Fragment>
 
     );
